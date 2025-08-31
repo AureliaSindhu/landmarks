fe7760999599fae65b130a5cc8f0289b7d91695c
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-08-31 22:26:30 +0700

    feat: view init

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by aacode on 8/31/25.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
