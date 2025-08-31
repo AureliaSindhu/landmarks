bcde3e9dfcbd923942da2825a358092927033bd4
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-08-31 22:07:30 +0700

    project init

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by aacode on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
