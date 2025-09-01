bbf1b0ba1d314911c5212225e4284565f78aad30
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-09-01 13:46:39 +0700

    update: fav toggle

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List (landmarks){ landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark:  landmark)
+            List{
+                Toggle ("Show Favorites Only", isOn: $showFavoritesOnly)
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark:  landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
+            .animation(.default, value: filteredLandmarks)
         } detail: {
             Text("Select a Landmark")
         }
