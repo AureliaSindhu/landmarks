524f64e08a94814921ab4c2480dfcb950440be42
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-09-02 18:14:00 +0700

    updated feature view ui page control logic

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,18 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by aacode on 9/2/25.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageControl()
+}
