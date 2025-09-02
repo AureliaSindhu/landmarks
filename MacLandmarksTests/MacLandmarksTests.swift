bf96c3d0761533d4b557246ea11b5908f447e909
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-09-02 19:25:56 +0700

    mac init updated icon

---
+++ MacLandmarksTests/MacLandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksTests.swift
+//  MacLandmarksTests
+//
+//  Created by aacode on 9/2/25.
+//
+
+import Testing
+@testable import MacLandmarks
+
+struct MacLandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
