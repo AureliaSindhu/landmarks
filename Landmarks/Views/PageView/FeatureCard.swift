92acaa8dbbc090c9725a6bc2f02e418eb219c75b
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-09-02 17:42:09 +0700

    view controller init swiping fcns

---
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,52 @@
+//
+//  FeaturedCard.swift
+//  Landmarks
+//
+//  Created by aacode on 9/2/25.
+//
+
+import SwiftUI
+
+struct FeaturedCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featureIamge?
+            .resizable()
+            .overlay{
+                TextOverlay(landmark: landmark)
+            }
+    }
+    
+    struct TextOverlay: View {
+        var landmark: Landmark
+
+
+        var gradient: LinearGradient {
+            .linearGradient(
+                Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
+                startPoint: .bottom,
+                endPoint: .center)
+        }
+
+
+        var body: some View {
+            ZStack(alignment: .bottomLeading) {
+                gradient
+                VStack(alignment: .leading) {
+                    Text(landmark.name)
+                        .font(.title)
+                        .bold()
+                    Text(landmark.park)
+                }
+                .padding()
+            }
+            .foregroundStyle(.white)
+        }
+    }
+}
+
+#Preview {
+    FeaturedCard(landmark: ModelData().features[0])
+        .aspectRatio(3/2, contentMode: .fit)
+}
---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PageViewControlle.swift
+//  Landmarks
+//
+//  Created by aacode on 9/1/25.
+//
+
+import SwiftUI
+
+struct PageViewControlle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageViewControlle()
+}
