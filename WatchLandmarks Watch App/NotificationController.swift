626166f20719419e6790800d6f3596fd8c2018b4
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-09-02 19:13:38 +0700

    updates: notif on watch watch completed

---
+++ WatchLandmarks Watch App/NotificationController.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationController.swift
+//  WatchLandmarks Watch App
+//
+//  Created by aacode on 9/2/25.
+//
+
+import SwiftUI
+
+struct NotificationController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationController()
+}
